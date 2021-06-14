ddfc59ce1828ee57292d3a523b270a6a94c90bac
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-06-14 18:57:21 +0300

    2uzduotis daugybos lentele

--- index_2021_06_14.js
+++ index_2021_06_14.js
@@ -5,26 +5,26 @@
 var skaicius = 2;
 
 while (skaicius <= 100) {
-    var arPirminis = true;
-    var atgal = skaicius - 1;
-    while (atgal >= 2 && arPirminis) {
-        if (skaicius % atgal === 0){
-            arPirminis = false;
+    var arPirminis = true; // pradzioj galvojam kad skaicius yra pirminis
+    var atgal = skaicius - 1; // nuo skaiciaus eisiu zemyn
+    while (atgal >= 2 && arPirminis) { // && arPirminis daugiau ciklo netikrina, kai skaicius jau tapo false // true ir false tampa false // true ir true tampa false ir vykdo salygas
+        if (skaicius % atgal === 0){ // ar dalinasi be liekanos
+            arPirminis = false; // jei dalinasi be liekanos arPirminis pasakem false, ne ne pirminis
             
         }
         atgal = atgal - 1;
     }
-    if (arPirminis) {
+    if (arPirminis) { // jeigu true vykdys sekancia salyga console.log
         console.log(skaicius);
     }
     skaicius = skaicius + 1;
 }
 
 /* skaicius: 2                          3              4
-2 <= 100 true
+2 <= 100 true                         3 <= 100 true
 arPirminis: true                        true            true
 atgal: skaicius - 1 /    2 - 1 = 1       2                3
-atgal >=2 / 1 >= 2 false
+atgal >=2 / 1 >= 2 false                 
 console.log atspausdina 2                3
 
 
@@ -42,7 +42,8 @@ var i = 1;
 
 while (i <= 10) {
     console.log(i);
-    i = i + 1; // sutrumpinimas i +=1 prie i prideda 1 // parasius +4 patikrina kas ketvira 
+    i = i + 1; // sutrumpinimas i +=1 prie i prideda 1 // parasius +4 patikrina kas ketvira ir kas ketvirta atspausdina
+    // i +=1 paima reiksme is i prideda 1 ir padeda reiksme i i 
 }
 
 
@@ -56,4 +57,14 @@ while (i <= 10) {
 */  
 
 
+var a = 15;
+
+do {    // do while pradzioje ivykdo, tada tikrina salygas jei ture eina toliau cikle, jei false nevykdo, bet viena karta tikrai bus atspausdinta reiksme
+    console.log(a);
+    a += 4;
+   
+}
+ while (a <= 10);
+
+ 
 
---
+++ uzduotis2.js
@@ -0,0 +1,17 @@
+// Daugybos lentele 
+
+var nuo = 1;
+var iki = 3; 
+
+// turi atspausdinti 1*1=1 1*2=2 1*3=3   2*1=2 2*2=4 2*3=6   3*1=3 3*2=6 3*3=9 rezultatus
+
+var i = nuo;
+
+while (i <= iki) {
+    var j = nuo;
+    while (j <= iki) {
+        console.log (i * j);
+        j = j + 1;
+    }
+    i = i + 1;
+}
