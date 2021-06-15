ca13b8523320013aa73f226e76db4db612f85431
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-06-15 12:43:54 +0300

    ++ operatorius

---
+++ index_2021_06_15.js
@@ -0,0 +1,50 @@
+"use strict"
+
+// operatorius ++, prideda per viena, labai panasus i +=1
+
+var sk = 3;
+var kitas = 5 + sk++;
+//              paima sk reiksme 3 ir atsimena
+//              padidina sk 1-u -> sk:4 // atsakyme cosnsole.log sk bus 4, nes padidino 
+//          5 + 3 / naudoja pirmine reiksme 3
+//          5 + 3
+//            8
+//        i kitas padedam 8
+
+var ska = 3;
+var kitas1 = 5 + ++ska;
+//               padidina ska 1-u -> ska:4
+//               paima ska reiksme 4
+//                5 + 4
+//                 9
+//              i kitas1 padeda 9
+
+
+console.log(sk);
+console.log(kitas);
+console.log(kitas1);
+
+
+
+
+
+
+
+
+var ski = 3;
+var kitas2 = 10 - ++ski + ski++ - ++ski;
+//                padidinam ski 1-u -> ski:4
+//                paimam sk reiksme 4
+//                         paimam ski reiksme 4 ir atsimena
+//                         padidina ski 1-u -> ski:5
+//                                  padidinam ski 1 -u -> 6
+//                                  paimam ski reiksme 6 
+//           10 - 4
+//              6      + 4
+//                 10 - 6
+//                      4             
+
+
+
+
+console.log(kitas2);
