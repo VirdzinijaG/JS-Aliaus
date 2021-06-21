d94ced444850c0330214ebf2a8e53740f6c00d5d
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-06-21 22:48:46 +0300

    funkcijos

--- index_2021_06_21.js
+++ index_2021_06_21.js
@@ -33,20 +33,48 @@ conssole.log (rez);
 
 let kamuoliukai = [3, 5.4, 2.3, 4.9, 0.1, 0.23];
 
+for (let i = 0; i < kamuoliukai.length - 1; i++) {
+    for (let j = i + 1; j < kamuoliukai.length; j++) {
+        if (4 * 3.14 * kamuoliukai[i] * kamuoliukai[i] * kamuoliukai[i] / 3 > 4 * 3.14 * kamuoliukai[j] * kamuoliukai[j] * kamuoliukai[j] / 3 ) {
+            let tmp = kamuoliukai[i];
+            kamuoliukai[i] = kamuoliukai[j];
+            kamuoliukai[j] = tmp;
+        } 
+    }
+}
 for (let i = 0; i < kamuoliukai.length; i++) {
-console.log(turis(kamuoliukai[i]));
+    console.log(4 * 3.14 * kamuoliukai[i] * kamuoliukai[i] * kamuoliukai[i] / 3);
+}
+// isvedami kamuoliuku turiai didejimo tvarka
+
+
+
+// isvedami kamuoliuku turiai didejimo tvarka naudojant funkcija
+// for (let i = 0; i < kamuoliukai.length - 1; i++) {
+//     for (let j = i + 1; j < kamuoliukai.length; j++) {
+//         if (turis(kamuoliukai[i]) > turis(kamuoliukai[j])) {
+//             let tmp = kamuoliukai[i];
+//             kamuoliukai[i] = kamuoliukai[j];
+//             kamuoliukai[j] = tmp;
+//         } 
+//     }
+// }
+
+// isvedami turiai naudojant funkcija
+for (let i = 0; i < kamuoliukai.length; i++) { // i++ atliekama paskutine kai ivykdomos salygos visos 
+console.log(turis(kamuoliukai[i])); // turis kvietimas funkcijos 
 }
 
-console.log(turis);
-console.log(turis.toString());
+console.log(turis); // kintamojo reiksme // nuoroda i funkcija [Function: turis]
+console.log(turis.toString()); // isveda visa funkcijos aprasyma
 
-function turis(r) {
+function turis(r) { // funkicja, funkcijos pavadinimas () skliausteliuose duomenys, {} aprasoma ka funkcija turi daryti  // r kintamasis
 let t = 4 * 3.14 * r * r * r / 3;
-return t;
+return t; // grazinimas
 }
 
-let didelisKamuolys = turis(945.78)
-console.log(didelisKamuolys);
+let didelisKamuolys = turis(945.78) // kintamajm priskirtas funkcija turis su reiksme (945.78) vietoj r
+console.log(didelisKamuolys); // isveda turio reiksme
 
 
 function suma (a, b) {
@@ -54,11 +82,11 @@ function suma (a, b) {
     return s;
 }
 
-let sk1 = suma(2,3);
+let sk1 = suma(2,3); // sumos funkcija su nurodytom  reiksmemis a ir b = 2, 3
 
 console.log(sk1);
 
-let sk2 = suma(10, 15);
+let sk2 = suma(10, 15); // sumos funkcija su nurodytom reiksmemis a ir b = 10, 15
 console.log(sk2);
 
 
@@ -72,7 +100,7 @@ function didesnioSkaiciausKvadratas (a, b) {
     return kv;
 }
 
-let dsk = didesnioSkaiciausKvadratas(5,3);
+let dsk = didesnioSkaiciausKvadratas(5,3); // funkcijos iskvietimas su nurodytomis reiksmemis a ir b = 5 , 3
 console.log(dsk);
 
 
