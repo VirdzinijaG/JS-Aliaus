214056086ae9b9dfb370ef1600f0fe3ea1efca5e
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-06-28 16:22:51 +0300

    Funkcija objekte

--- 2021_06_28_objekte_funkcija_konstruktyvi_funkcija.js
+++ 2021_06_28_objekte_funkcija_konstruktyvi_funkcija.js
@@ -1,18 +1,18 @@
 "use strict"
 
 // funkcijos objektuose
-let masina1 = {
+let masina1 = { // masina1 reiksme objektas
     pavadinimas: "pirma",
     kelias: 0,
     greitis: 0,
-    gazas: function(kiek) { // funkcija objekte
-        this.greitis += kiek;
-    },
+    gazas: function(kiek) { // funkcija objekte // funkcija be vardo
+        this.greitis += kiek; // this kintamasis, rodo i objekta su kuriuo siuo metu dirba
+    }, // this.greitis yra sio objekto greitis, kuris pakeiciamas naudojant funkcija
     stabdis: function(kiek) {
         this.greitis -= kiek;
         if (this.greitis < 0) {
             this.greitis = 0;
-        }
+        } // gazas ir stabdis reiksme funkcija
     }
 };
 
@@ -34,8 +34,8 @@ let masina2 = {
 console.log(masina1);
 console.log(masina2);
 
-masina1.gazas(15);
-masina2.gazas(3);
+masina1.gazas(15); // funkcijos kvietimas // 15 bus reiksme funkcijos gazas (kiek)
+masina2.gazas(3); 
 console.log(masina1);
 console.log(masina2);
 
