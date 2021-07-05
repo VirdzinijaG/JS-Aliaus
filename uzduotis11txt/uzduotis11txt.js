67b94faa727c4f3b4e385909d7cf33f8fb801f4e
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-07-05 12:15:45 +0300

    txt uzduotis

--- uzduotis11txt/uzduotis11txt.js
+++ uzduotis11txt/uzduotis11txt.js
@@ -12,42 +12,70 @@ Ta string'a suskaldyti zodziais (per tarpa). Atspausdinti kiekviena zodi ilgejim
 Po to perskaityti failus: 5, 2. Atspausdinti ju turini 
 */
 
-const fs = require ("fs");
+
+// const fs = require ("fs");
 
 
-let first = "";
-let second = "";
-let last = "";
-let all;
+// let first = "";
+// let second = "";
+// let last = "";
+// let all;
+
+// fs.readFile("uzduotis11txt/4.txt", (err, data) => {
+//     first += data;
+//     // console.log(first);
+//         fs.readFile("uzduotis11txt/3.txt", (err, data) => {
+//             second += data;
+//         // console.log(second);
+//             fs.readFile("uzduotis11txt/1.txt", (err, data) => {
+//                 last += data
+//             // console.log(last);
+//             all = (first + " "  + second + " "  + last);
+//             console.log(all);
+//             let z = all.split(" ")
+//             for (var i = 0; i < z.length - 1; i++) {
+//                 z[i] += " ";
+//                 console.log(z);
+//                 z.sort((a, b) => {
+//                     return a.length - b.length
+//                 })
+//             }
+//             console.log(z);
+//             });
+//             fs.readFile("uzduotis11txt/5.txt", (err, data) => {
+//                 console.log(" " + data);
+//             });
+//             fs.readFile("uzduotis11txt/2.txt", (err, data) => {
+//                 console.log(" " + data);
+//             });
+//         });
+//     });
+
+
+
+
+// per pamoka parodytas
+
+const fs = require("fs");
+
+let s = "";
 
 fs.readFile("uzduotis11txt/4.txt", (err, data) => {
-    first += data;
-    // console.log(first);
-        fs.readFile("uzduotis11txt/3.txt", (err, data) => {
-            second += data;
-        // console.log(second);
-            fs.readFile("uzduotis11txt/1.txt", (err, data) => {
-                last += data
-            // console.log(last);
-            all = (first + " "  + second + " "  + last);
-            console.log(all);
-            let z = all.split(" ")
-            for (var i = 0; i < z.length - 1; i++) {
-                z[i] += " ";
-                console.log(z);
-                z.sort((a, b) => {
-                    return a.length - b.length
-                })
-            }
-            console.log(z);
-            });
+    s += data;
+    fs.readFile("uzduotis11txt/3.txt", (err, data) => {
+        s += " " + data;
+        fs.readFile("uzduotis11txt/1.txt", (err, data) => {
+            s += " " + data;
+            console.log(s);
+            let m = s.split(" ");
+            m.sort((e1, e2) => e1.length - e2.length);
+            console.log(m);
             fs.readFile("uzduotis11txt/5.txt", (err, data) => {
-                console.log(" " + data);
-            });
-            fs.readFile("uzduotis11txt/2.txt", (err, data) => {
-                console.log(" " + data);
+                console.log("" + data);
+                fs.readFile("uzduotis11txt/2.txt", (err, data) => {
+                    console.log("" + data);
+                });
             });
         });
     });
-
-   
+});
